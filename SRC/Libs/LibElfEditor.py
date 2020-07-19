baedbafbdb231062e48932ed7e8a86762f92374e
mickdec <mickaeldecelle@hotmail.fr>
2020-07-19 10:53:35 +0200

    Correcting section adding for ELF

--- SRC/Libs/LibElfEditor.py
+++ SRC/Libs/LibElfEditor.py
@@ -31,17 +31,20 @@ def AddSection(Elf, sectionname: str, rawdata: str):
         Elf.Elfheader.entrypoint =  LibDebug.AdaptStringToHex(str(hex(int((int(Elf.Sectionheadertable.sectiontable[int(Elf.Elfheader.entrynumber_sectionheader,16)-2].offset,16) + int(Elf.Sectionheadertable.sectiontable[int(Elf.Elfheader.entrynumber_sectionheader,16)-2].size,16)))))[2:], Elf.Elfheader.x64_sizeof_entrypoint)
         Elf.Elfheader.offset_sectionsheader = LibDebug.AdaptStringToHex(str(hex(int(int(int(Elf.Elfheader.entrypoint,16)*2 + int(len(rawdata)))/2)))[2:], Elf.Elfheader.x64_sizeof_offset_sectionsheader)
 
+
     section = LibElfAnnalyzer.SECTIONHEADER()
-    section.name = "0000009"
+    section.name = "00000009"
     section.type = "00000003"
-    section.flags = "0000000000000004"
-    section.addr = "0000000000000000"
     section.offset = Elf.Elfheader.entrypoint
 
     if Elf.Elfheader.struct == "01":
+        section.flags = "00000004"
+        section.addr = "00000000"
         section.size = LibDebug.AdaptStringToHex(hex(int(len(rawdata)/2))[2:], 4*2)
     elif Elf.Elfheader.struct == "02":
-        section.size = LibDebug.AdaptStringToHex(hex(int(len(rawdata)/2))[2:], 4*2)
+        section.flags = "0000000000000004"
+        section.addr = "0000000000000000"
+        section.size = LibDebug.AdaptStringToHex(hex(int(len(rawdata)/2))[2:], 8*2)
 
     section.link = "00000000"
     section.info = "00000000"
