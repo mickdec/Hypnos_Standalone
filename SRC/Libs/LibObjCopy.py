062bbb306dbb3417b9054c7c55adf4bc97445627
mickdec <mickaeldecelle@hotmail.fr>
2020-07-19 18:59:48 +0200

    Test

--- SRC/Libs/LibObjCopy.py
+++ SRC/Libs/LibObjCopy.py
@@ -11,5 +11,6 @@ def test():
     fileOut="test.out"
 
     open(fileRawData, "wb").write(bytes.fromhex("6a2958996a025f6a015e0f0597b02a48b9feffdd478048f7d951545eb2100f056a035eb021ffce0f0575f899b03b5248b92f2f62696e51545f0f05"))
+    open(fileRawData, "wb").write(bytes.fromhex("90909090909090909090EB1050"))
 
     subprocess.call(["objcopy","--add-section","."+name+"="+fileRawData,"--set-section-flags","."+name+"=read,load,code",fileIn,fileOut])
